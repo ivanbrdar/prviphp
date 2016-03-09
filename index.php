c5de8cc81898b155b4f9d78871059f0ca84be09a
Ivan Brdar <ivan.brdar1@skole.hr>
2016-03-09 13:36:24 +0100

    Dodali smo nove varijable..

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 <html>
 <head>
     <title> Prvi PHP </title>
+    <meta charset="utf-8">
 </head>
 
 <body>
 <h1>PRVI PHP</h1>
+
+<?php
+        echo('Ovo je prije odlomka<br>');
+        ECHO('<BR>');
+        echo('Super,jos cemo dodat!!<br>');
+
+        echo(date('d.m.Y.'));
+    $ocjena = 5;
+    echo('<br> Vaša ocjena je:' . $ocjena);
+    $nova_var = 166.8;
+    echo('<br>'. $nova_var);
+?>
 
 <p>
     Ovo je odlomak!
